27f53ee479e1778554856c26b9c106d88dba1793
AjaniBrowne <ajani.browne@yahoo.com>
2022-07-31 21:35:50 -0400

    Commit on Main "complete routes"

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -29,7 +29,7 @@ Product.init(
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      default: [10],
+      defaultValue: 10,
       validate:{
         isNumeric: true,
       },
@@ -37,7 +37,7 @@ Product.init(
     category_id:{
       type: DataTypes.INTEGER,
       references: {
-        model:'catergory',
+        model:'category',
         key:'id',
       },
     }
